2ff1bed0027b923b5faa9b1c41b5f2a29ecb9e88
ecolosseum24 <ecolosseum24@gmail.com>
2024-07-03 22:25:37 +0530

    Update event.js

--- script/event.js
+++ script/event.js
@@ -7,16 +7,14 @@ function addCard(name, eligibility, description, banner, participants, url) {
   card.innerHTML = `
     <div class="card-inner border-2 border-white border-opacity-60 rounded-lg">
       <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="${banner}" alt="banner" />
-      <div class="p-6 flex flex-col justify-between">
-        <div>
+      <div class="p-6">
           <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
             GRADES: ${eligibility.join(" - ")}
           </h2>
           <h1 class="title-font text-lg font-medium text-gray-300 mb-3">${name}</h1>
           <p class="leading-relaxed mb-3 text-gray-400 description">${description}</p>
-        </div>
-        <div class="flex items-center justify-between">
-          <a class="text-purple-600 inline-flex items-center md:mb-2 lg:mb-0 register" href="${url}" target="_blank">
+        <div class="flex items-center justify-between register">
+          <a class="text-purple-600 inline-flex items-center md:mb-2 lg:mb-0" href="${url}" target="_blank">
             Register Now
             <svg
               class="w-4 h-4 ml-2"
