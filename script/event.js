7cfc175a487e885841dcd4839e6801387c34f5df
ecolosseum24 <ecolosseum24@gmail.com>
2024-07-03 12:38:34 +0530

    Update event.js

--- script/event.js
+++ script/event.js
@@ -21,7 +21,7 @@ function addCard(name, eligibility, description, banner, participants, url) {
     <h1 class="title-font text-lg font-medium text-gray-300 mb-3">
       ${name}
     </h1>
-    <div>
+    <div class="coolcard">
     <p class="leading-relaxed mb-3 text-gray-400">
       ${description}
     </p>
