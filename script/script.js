dec040c0eaa55acde4f04d6d9752e54a47751391
ecolosseum24 <ecolosseum24@gmail.com>
2024-07-03 10:41:49 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -38,7 +38,7 @@ let flick = function() {
 
 }
 
-var countDownDate = new Date("July 02, 2024 09:00:00").getTime();
+var countDownDate = new Date("July 04, 2024 10:43:00").getTime();
 
 var x = setInterval(function () {
   var now = new Date().getTime();
