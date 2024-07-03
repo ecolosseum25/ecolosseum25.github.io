d1301c79d68e5cc0bcfa431cda9ff475a1c7fae7
ecolosseum24 <ecolosseum24@gmail.com>
2024-07-03 20:21:26 +0530

    Update event.js

--- script/event.js
+++ script/event.js
@@ -40,27 +40,126 @@ function addCard(name, eligibility, description, banner, participants, url) {
 }
 
 const events = [
-  {
+ {
     name: "Tech Attire",
     eligibility: [2, 3],
-    description:
-      "Participants craft costumes inspired by e-waste, merging creativity with tech, judged on innovation and thematic relevance to electronic waste.",
-    banner:
-      "https://img.freepik.com/premium-photo/people-cyber-reality_622301-1398.jpg",
+    description: "Participants craft costumes inspired by e-waste, merging creativity with tech, judged on innovation and thematic relevance to electronic waste.",
+    banner: "https://img.freepik.com/premium-photo/people-cyber-reality_622301-1398.jpg",
     participants: 1,
-    url: "https://forms.office.com/pages/responsepage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRUOTVYTFQ0NTlDQVQyOVVZMTdYV1UyUE1BUy4u",
+    url: "https://forms.office.com/pages/responsepage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRUOTVYTFQ0NTlDQVQyOVVZMTdYV1UyUE1BUy4u"
   },
   {
     name: "DOCUDRAMA",
     eligibility: [4, 5],
-    description:
-      "Unleash your creativity! Join our film making competition and let your story shine",
-    banner:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlI9obEIzcjU0nDbg-r28asdztvKV5w95dSg&usqp=CAU",
+    description: "Unleash your creativity! Join our film making competition and let your story shine.",
+    banner: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlI9obEIzcjU0nDbg-r28asdztvKV5w95dSg&usqp=CAU",
     participants: 2,
-    url: "https://forms.office.com/pages/responsepage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRURFpGWDZHUlNaWksxTTA1OE42SkVEQlVBNS4u",
+    url: "https://forms.office.com/pages/responsepage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRURFpGWDZHUlNaWksxTTA1OE42SkVEQlVBNS4u"
   },
-  // Add more events as needed
+  {
+    name: "CodeCraftGaming",
+    eligibility: [6, 8],
+    description: "Competitors develop arcade games envisioning sustainable cities, blending creativity with future-forward environmental solutions.",
+    banner: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Frockymountevents.com%2Fwp-content%2Fuploads%2F2019%2F11%2FPerson-playing-an-arcade-game.jpg&f=1&nofb=1&ipt=8b1500372364ff77de787dc2b43555a66c9ead59c5ec12b13cb07cff4eb15330&ipo=images",
+    participants: 2,
+    url: "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRUMUJENThBMjJJRUNDVDRSMTZSNTJRUDg5VS4u"
+  },
+  {
+    name: "ANIMATION-X",
+    eligibility: [8, 9],
+    description: "Get ready to animate the future of climate action! Participants create thrilling videos on climate change, exploring causes, impacts, and revolutionary solutions.",
+    banner: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkeW_AwOm6RuF9TNrodSSGs3Jafv4j8p8PNA&s",
+    participants: 2,
+    url: "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRUMUJENThBMjJJRUNDVDRSMTZSNTJRUDg5VS4u"
+  },
+  {
+    name: "BOT-BRAWL",
+    eligibility: [6, 8],
+    description: "Seize the reins in electrifying robot showdowns, strategizing through each round to dominate and secure a thrilling path to the high-stakes final battles.",
+    banner: "https://t3.ftcdn.net/jpg/01/80/97/92/360_F_180979216_dsMX0jqo0MrflzwSs86rQuIrxVHbbPW5.jpg",
+    participants: "3",
+    url: "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRUN1E4QlhFNTZUQ1k1T05TUVYzUTA0TlVOSy4u"
+  },
+  {
+    name: "BLENDRUSH",
+    eligibility: [9, 10],
+    description: "Students embark on an exhilarating hunt across Google and YouTube for captivating video clips, showcasing creativity and storytelling prowess in a thrilling competition.",
+    banner: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkC8WAQAfGvX5rep_llJ8K8KnaeZ1yYpaIOw&s",
+    participants: 1,
+    url: "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRUNjQxS1pZWVdXSzJTRDJYQlZEM1FGQzI3QS4u"
+  },
+  {
+    name: "3DesignDynamic",
+    eligibility: [9, 10],
+    description: "Pioneering the future of space fashion, participants create cutting-edge wearables blending style and functionality for life among the stars!",
+    banner: "https://cdn.dribbble.com/users/4823084/screenshots/18863071/media/78f0367c4d388ee5e37524cee27cf1ee.png?compress=1&resize=400x300",
+    participants: 2,
+    url: "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRUN0xDNUpJRTExTUNTTUVQQzZOTVRZR1lMNC4u"
+  },
+  {
+    name: "DigitalDreamscapes",
+    eligibility: [9, 12],
+    description: "Get creative and showcase your skills in our thrilling Photoshop competition",
+    banner: "https://i.ytimg.com/vi/l7r4EdSGrbk/maxresdefault.jpg",
+    participants: 2,
+    url: "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRUODNROUkxWEZPQ0dRVjdOMUtTWEFYREdOTS4u"
+  },
+  {
+    name: "App-A-Thon",
+    eligibility: [11, 12],
+    description: "Students will build an app to make every citizen in India adopt an environment conscious lifestyle.",
+    banner: "https://i.imgur.com/nuXeEj9.jpg",
+    participants: 2,
+    url: "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRUNU1FTTlQTUNDMkRRVjgwM0tGRjQ1VVNQMS4u"
+  },
+  {
+    name: "Perspective",
+    eligibility: [9, 12],
+    description: "Innovating solutions for any community problem using microcontroller and AI tool",
+    banner: "https://www.electronicaembajadores.com/datos/fotos/articulos/grandes/lc/lca1/lca1k81.jpg",
+    participants: 2,
+    url: "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRUNU1FTTlQTUNDMkRRVjgwM0tGRjQ1VVNQMS4u"
+  },
+  {
+    name: "ELEV8PITCH",
+    eligibility: [9, 12],
+    description: "Students ignite innovation with tech-driven business pitches aligned to Sustainable Development Goals (SDGs), shaping a sustainable future with global impact.",
+    banner: "https://soutienaerrideluca.net/wp-content/uploads/2017/09/entrepreneurship.jpg",
+    participants: 4,
+    url: "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRUMEYwRkNGQlZIS09RU0k4R0w0VlAxWkhDMS4u"
+  },
+  {
+    name: "Decode to Learn: Virtual Escape Room Challenge",
+    eligibility: [9, 12],
+    description: "Students engage their intellect in virtual escape rooms, solving puzzles and unraveling mysteries to break free within a challenging and immersive environment.",
+    banner: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fd36tnp772eyphs.cloudfront.net%2Fblogs%2F1%2F2018%2F08%2FEscape-room.jpg&f=1&nofb=1&ipt=c7750d7e9aa0efa5aa1e358aa6d50dd57e402b1a0db1a6a07fc7477e91f0b6c1&ipo=images",
+    participants: 4,
+    url: "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRUN0dRTVIySkY2MkRCRFFBWU5CMlc5MVk3Ry4u"
+  },
+  {
+    name: "TECHTRIVIA",
+    eligibility: [9, 12],
+    description: "Students test their knowledge and problem-solving skills with a series of intense technology-related questions, aiming to prove their mastery in a competitive setting.",
+    banner: "https://miro.medium.com/v2/resize:fit:3720/1*_PtTP7XVXZxPStTVMmvKRg.jpeg",
+    participants: 1,
+    url: "https://forms.gle/HUWwtThzGS5XoxhB6"
+  },
+  {
+    name: "Playoffs",
+    eligibility: [6, 12],
+    description: "Show off your flow of ideas via your moves and let the world recognise your talent",
+    banner: "https://i.ytimg.com/vi/Zfsg3oiPXGc/maxresdefault.jpg",
+    participants: "5",
+    url: "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRUMUk3T0VVSTVMN05FWkJGVFowM0dJR01VSS4u"
+  },
+  {
+    name: "ByteBeat",
+    eligibility: [8, 12],
+    description: "Show off your flow of ideas via your moves and let the world recognise your talent",
+    banner: "https://www.noblemotiondance.com/wp-content/uploads/2022/01/NobleMotion-Dance-MAN-OVERBOARD-Photographer-Lynn-Lane-WEB-4-600x400.jpg",
+    participants: "8-10",
+    url: "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAHf2GwRUOTI3RUowWVRGVk44TllZS1pVU0MwR1FFMi4u"
+  }
 ];
 
 events.forEach((event) => {
